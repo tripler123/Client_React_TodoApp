d8283bacae8a86f11b1dae5a405dddb8aabf2a12
Renzo <renzo@MacBook-Pro-de-Renzo.local>
2018-12-14 01:14:43 -0500

    finish app

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// data
 import { todos } from './todos.json';
- console.log(todos);
- 
+
+// subcomponents
+import TodoForm from './components/TodoForm';
+
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       todos
     }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
+
+  removeTodo(index) {
+    if (window.confirm('Are you sure you want delete it?')) {
+      this.setState({
+        todos: this.state.todos.filter((e, i) => {
+          return i !== index
+        })
+      });
+    }
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
   render() {
-    const todos = this.state.todos.map((todo, i) =>{
-      return(
+    const todos = this.state.todos.map((todo, i) => {
+      return (
         <div className="col-md-4" key={i}>
           <div className="card mt-4">
             <div className="card-title text-center">
@@ -26,26 +47,46 @@ class App extends Component {
             <div className="card-body">
               {todo.description}
             </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+            </div>
           </div>
         </div>
       )
-    })
+    });
+
+    // RETURN THE COMPONENT
     return (
       <div className="App">
-        <nav className="navbar navbar-dark bg-dark"> 
-            <a href="#" className="text-white">
-              Tasks
-              <span className="badge badge-pill badge-light  ml-2 ">
-                { this.state.todos.length }
-              </span>
-            </a>  
-          </nav>
-          <div className="container">
-            <div className="row mt-4">
-              { todos }
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>
             </div>
           </div>
-        <img src={logo} className="App-logo" alt="logo" />
+        </div>
       </div>
     );
   }
